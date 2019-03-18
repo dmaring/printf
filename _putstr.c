67f3b76398a6683e3c1a3167534a5abd594d37a3
Drew <andrew.maring@gmail.com>
2019-03-18 17:25:10 +0000

    Add supporting functions

---
+++ _putstr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _putstr: function that prints string to stdout
+ * @s: input string as char pointer
+ *
+ * Return: integer number of characters printed
+ */
+int _putstr(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (i);
+}
---
+++ va_print_c.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * va_putchar - get value with va_arg and then _putchar
+ * @valist: pointer to memory location
+ *
+ * Return: number of characters printed
+ */
+int va_print_c(va_list valist)
+{
+	return (_putchar(va_arg(valist, int)));
+}

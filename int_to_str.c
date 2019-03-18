627295542acbb672ec3183331dcc54be7efe653e
snehadasa <sneha.dasasneha@gmail.com>
2019-03-18 10:09:03 -0700

    integer to string

--- int_to_str.c
+++ int_to_str.c
@@ -1,52 +1,72 @@
 #include "holberton.h"
+#include <stdio.h>
 
-/**
- * strlen_1 - gets length of the string
- * @s: input string
- * Return: length of string
- */
-int _strlen(char *s)
+int strlen_1(char *s)
 {
 	int i;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; s[i]; i++)
 		;
 	return (i);
 }
 
-void i_to_str(int num, char str[])
+/**
+ * i_to_str - converts integer to string.
+ * @num: number to be converted.
+ * Return: returns the string.
+ */
+char * i_to_str(int num, int base)
 {
-	int i = 0,temp, rem, j, sign;
+	int i = 0, j, rem = 0, rev = 0, length = 0, sign = 1, temp;
+	char *result;
 
-	if (sign == -1)
+	if (num == 0)
 	{
-		num = -num;
+		result = malloc(sizeof(char) * 2);
+		result[0] = '0';
+		result[1] = '\0';
+		return (result);
 	}
+
+	if (num < 0)
+	{
+		sign = -1;
+		num = -num;
+		length++;
+	}
+
 	while (num > 0)
 	{
-		rem = num % 10;
-		num = num / 10;
-
+		rem = num % base;
+		num = num / base;
+		rev = rev * base + rem;
 		if (rem > 9 && rem < 16)
-		{
-			str[i++] = rem -10 + 'A';
-		}
-		else
-		{
-			str[i++] = rem + '0';
-		}
+                {
+                        result[i++] = rem - 10 + 'A';
+                }
+                else
+                {
+                        result[i++] = rem + '0';
+                }
+
+		length++;
 	}
+	printf("length: %d\n", length);
+
+	result = malloc(sizeof(char) * (length + 1));
 
 	if (sign == -1)
 	{
-		str[i++] = '-';
+		result[i++] = '-';
 	}
-	str[i] = '\0';
 
-	for (i = 0, j = strlen_1(str) - 1; i < j; i++, j--)
+	result[i] = '\0';
+
+	for (i = 0, j = strlen_1(result) - 1; i < j; i++, j--)
 	{
-		temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
+		temp = result[i];
+		result[i] = result[j];
+		result[j] = temp;
 	}
+	return (result);
 }

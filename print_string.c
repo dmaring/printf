0fd91a2a1c462e445bcb119db22865a634193a02
snehadasa <sneha.dasasneha@gmail.com>
2019-03-18 16:13:08 -0700

    print_string.c

--- print_string.c
+++ print_string.c
@@ -1,6 +1,11 @@
 #include "holberton.h"
 #include <unistd.h>
 
+/**
+ * print_string - prints string.
+ * @s: string to be printed.
+ * Return: returns value of the string.
+ */
 int print_string(char *s)
 {
 	int i = 0;

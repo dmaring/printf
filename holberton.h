81be3c2d4756a04ab43e0812093999e364947cec
snehadasa <sneha.dasasneha@gmail.com>
2019-03-18 10:08:38 -0700

    holberton.h

---
+++ holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+int strlen_1(char *s);
+
+char * d_to_str(double num);
+
+char * i_to_str(int num, int base);
+
+int print_string(char *s);
+
+#endif

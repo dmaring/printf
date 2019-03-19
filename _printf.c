9b138f0e2b75d380bb64dc98e5f2e088056efce2
Drew <andrew.maring@gmail.com>
2019-03-19 00:34:34 +0000

    Fix problem not printing last character

--- _printf.c
+++ _printf.c
@@ -34,7 +34,6 @@ int _printf(const char *format, ...)
 			if (ptr)
 			{
 				ptr(valist);
-				i++;
 			}
 			else
 			{
--- test/main.c
+++ test/main.c
@@ -16,16 +16,15 @@ int main(void)
 	void *addr;
 
 
-
+/**
 	_printf("string: %s %s", "This is string 1\n", " and this is string 2\n");
 	_putchar('\n');
-	_printf("%c", c);
+	_printf("%c\n", c);
 	_putchar('\n');
-
-
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
+**/
 	_printf("Length:[%f, %i]\n", 40.23, 233);
+	_printf("String: %s\n", "Hello there");
+	_printf("Character: %c\n", 'c');
 
 	return (0);
 }
